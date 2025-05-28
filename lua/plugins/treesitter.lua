47f58962c7507b0220ecee3d0b6cd63b17e820cf
Phong Tran <540580+phongse@users.noreply.github.com>
2025-05-28 22:56:54 +0200

    refactor treesitter plugin spec

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,19 +3,28 @@ return {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
         branch = "main",
-        build = {
-            -- Requires manual install of new parsers as this is only triggered
-            -- on nvim-treesitter update
-            ":TSInstall dockerfile",
-            ":TSInstall json",
-            ":TSInstall lua",
-            ":TSInstall markdown",
-            ":TSInstall python",
-            ":TSInstall regex",
-            ":TSInstall toml",
-            ":TSInstall yaml",
-            ":TSUpdate",
-        },
+        build = ":TSUpdate",
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local filetype = args.match
+                    local lang = vim.treesitter.language.get_lang(filetype)
+                    if
+                        vim.tbl_contains(
+                            require("nvim-treesitter.config").get_available(),
+                            lang
+                        )
+                    then
+                        require("nvim-treesitter").install(lang):await(function()
+                            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                            vim.bo.indentexpr =
+                                "v:lua.require'nvim-treesitter'.indentexpr()"
+                            vim.treesitter.start()
+                        end)
+                    end
+                end,
+            })
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-context",
